c2423b15ad5bb9b709c7f43f8d9de0b32ccfc446
taajuddin <taajraza@gmail.com>
2020-08-20 21:15:14 +0530

    Updates

---
+++ precache-manifest.8f2dce6cfccfba113954b6c48bd87c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a49fbcfde212d5ddb723717c4a69e2",
+    "url": "/finder-github/index.html"
+  },
+  {
+    "revision": "2c07b36c2b31031700c7",
+    "url": "/finder-github/static/css/main.43f34a28.chunk.css"
+  },
+  {
+    "revision": "df62ea6b69a985ca5308",
+    "url": "/finder-github/static/js/2.b6b02351.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/finder-github/static/js/2.b6b02351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c07b36c2b31031700c7",
+    "url": "/finder-github/static/js/main.d9d342ba.chunk.js"
+  },
+  {
+    "revision": "5bdd6c71e41452e854c8",
+    "url": "/finder-github/static/js/runtime-main.16007679.js"
+  }
+]);
